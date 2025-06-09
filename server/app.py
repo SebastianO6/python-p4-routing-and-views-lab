1feec18bb82bd596201719a9c2e52542373e910e
Bill <bill.omondi@student.moringaschool.com>
2025-06-09 22:32:56 +0300

    python routing

--- server/app.py
+++ server/app.py
@@ -4,5 +4,51 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f"{parameter}"
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = [str(num) for num in range(parameter)]
+    output = '\n'.join(numbers) + '\n'
+    return output
+
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    if operation == '+':
+        result = num1 + num2
+    
+    elif operation == '-':
+        result = num1 - num2
+    
+    elif operation == '*':
+        result = num1 * num2
+
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division error"        
+
+    elif operation == '%':
+        if num2 != 0:
+            result = num1 % num2 
+        else:
+            return "Error: modulo zero" 
+
+    else:
+        return "Operation error: must be either +, -, *, %, or div"
+
+    return str(result)       
+          
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
